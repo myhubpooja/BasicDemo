efa6d3070bbe7a941d50a5247edce1eb76f1e5a0
pooja <poojaapatil9@gmail.com>
2023-01-18 16:18:13 +0530

    hello

---
+++ src/com/sample/Test.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("java");
+		list.add("Spring");
+		list.add("hibernate");
+
+		System.out.println(list);
+	}
+}
